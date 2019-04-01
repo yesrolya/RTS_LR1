0644ba5a0d5f66b2a834367edadb9443fc5c9a3c
yesrolya <yesrolya@yandex.ru>
2019-04-02 02:13:50 +1000

    12345

---
+++ RTS_LR1/ClassDateTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS_LR1
+{
+    class ClassDateTime
+    {
+        public void WorkWithDateTime (System.Windows.Controls.TextBlock n1, System.Windows.Controls.TextBlock n2)
+        {
+            //статические поля класса DateTime
+            var current = DateTime.Now; //текущее время ПК в его часовом поясе
+            var currentUTC = DateTime.UtcNow; //показывает время универсальное глобальное (UTC)
+            var today = DateTime.Today; //текущая дата без времени
+            var min = DateTime.MinValue; //минимальное значение структуры DateTime
+            var max = DateTime.MaxValue; //минимальное значение структуры DateTime
+            bool isLeap = DateTime.IsLeapYear(2019); //высокосный год или нет
+            int daysInMonth = DateTime.DaysInMonth(2019, 4); //возвращает количество дней в месяце
+
+            //основные конструкторы
+            var dt1 = new DateTime(100000000000); //число тактов, прошедшее от минимального значения
+            var dt2 = new DateTime(1,1,1); //дата
+            var dt3 = new DateTime(1, 1, 1, 0, 0, 1); //дата и время
+            var ticks = dt3.Ticks; //1 tick = 1E-7, отсчитывается для заданной даты от DateTime.MinValue
+
+            var cDate = current.Date;
+            //составные части: current.Day, current.Month, current.Year
+            var cTime = current.TimeOfDay; //точность до 1E-7 секунды, время от начала дня
+            //составные части: current.Hour, current.Minute, current.Second, current.Millisecond 
+
+            var cDayWeek = current.DayOfWeek;
+            var cDayYear = current.DayOfYear;
+
+            //преобразование строк в дату
+            string dtstr1 = "3/30/2019";
+            string dtstr2 = "4.30.2020 12:38:12";
+            var parse1 = DateTime.Parse(dtstr1);
+            DateTime parse2;
+            DateTime.TryParse(dtstr2, out parse2);
+
+            TimeSpan dto = parse2 - parse1;
+
+            TimeSpan ts = new TimeSpan(4, 4, 0);
+            var q1 = current.AddSeconds(1);
+            parse1.ToString("yyyy-mm =dd hh:mm-ss");
+        }
+    }
+}
--- RTS_LR1/MainWindow.xaml.cs
+++ RTS_LR1/MainWindow.xaml.cs
@@ -1,28 +1,159 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RTS_LR1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        DateTime currentTime;
+        TimeSpan ts;
+        DateTime date_diplom;
+        TimeSpan start_programm;
+        private System.Timers.Timer aTimer;
+        private System.Windows.Threading.DispatcherTimer bTimer;
+        private System.Windows.Forms.Timer cTimer;
+        bool visibleImg0 = true;
+        int h, w;
+        void Initialize()
+        {
+            Img0.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/1.png"));
+            Img2.Source = new BitmapImage(new Uri(@"pack://application:,,,/Resources/3.png"));
+            Img2.Stretch = Img0.Stretch = System.Windows.Media.Stretch.Uniform;
+            Img2.Visibility = Img0.Visibility = Visibility.Visible;
+            Img2.Height = Img0.Height = 200;
+            h = (int)Img2.Height;
+            w = (int)Img2.Width;
+            NowDateTime.Text = DateTime.Now.ToString("hh:mm:ss MM/dd/yyyy ") + DateTime.Now.DayOfWeek.ToString() ; 
+        }
+
+        void StartMyTimer ()
+        {
+            start_programm = new TimeSpan(0);
+            date_diplom = new DateTime(2019, 7, 7); //дата защиты диплома
+            long nowTicks = DateTime.Now.Ticks;
+            currentTime = new DateTime(nowTicks - nowTicks % 10000000);
+
+            Set_aTimer();
+            Set_bTimer();
+            Set_cTimer();
+            Set_dTimer();
+        }
+        
+        private void Set_aTimer()
+        {
+            //таймер этого типа не может работать с GUI выводится в консоли
+            aTimer = new System.Timers.Timer()
+            {
+                Interval = 1000,
+                AutoReset = true,
+                Enabled = true
+            };
+            aTimer.Elapsed += new ElapsedEventHandler(
+            delegate (Object source, System.Timers.ElapsedEventArgs e) 
+            {
+                currentTime = currentTime.AddSeconds(1);
+                TimeSpan temp = (date_diplom - currentTime);
+                Console.WriteLine(temp);
+            });
+        }
+
+        private void Set_bTimer()
+        {
+            //таймер WPF
+            ts = new TimeSpan(0, 0, 1);
+            bTimer = new System.Windows.Threading.DispatcherTimer();
+            bTimer.Tick += (sender, e) =>
+            {
+                if (visibleImg0)
+                    Img0.Visibility = Visibility.Hidden;
+                else
+                    Img0.Visibility = Visibility.Visible;
+                visibleImg0 = !visibleImg0;
+                start_programm += ts;
+                StartTime.Text = start_programm.ToString();
+            };
+            bTimer.Interval = ts;
+            bTimer.Start();
+        }
+        
+        private void Set_cTimer()
+        {
+            //таймер WindowsForms
+            cTimer = new System.Windows.Forms.Timer();
+            cTimer.Tick += (sender, e) =>
+            {
+                TimeSpan temp = (date_diplom - currentTime);
+                DiplomTime.Text = temp.ToString();
+            };
+            cTimer.Interval = 1000;
+            cTimer.Start();
+        }
+
+        private void Set_dTimer()
+        {
+            DoubleAnimation img2AnimateHeight = new DoubleAnimation
+            {
+                From = h,
+                To = h - 20,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(20)
+            };
+            DoubleAnimation img2AnimateWidth = new DoubleAnimation
+            {
+                From = w,
+                To = w - 20,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(20)
+            };
+            Img2.BeginAnimation(Image.HeightProperty, img2AnimateHeight);
+            Img2.BeginAnimation(Image.WidthProperty, img2AnimateWidth);
+        }
+
+        public struct SystemTime
+        {
+            public ushort Year;
+            public ushort Month;
+            public ushort DayOfWeek;
+            public ushort Day;
+            public ushort Hour;
+            public ushort Minute;
+            public ushort Second;
+            public ushort Millisecond;
+        };
+        [DllImport("kernel32.dll", EntryPoint = "GetSystemTime", SetLastError = true)]
+        public extern static void Win32GetSystemTime(ref SystemTime sysTime);
+
+        [DllImport("kernel32.dll", EntryPoint = "SetSystemTime", SetLastError = true)]
+        public extern static bool Win32SetSystemTime(ref SystemTime sysTime);
+
+        void SetNewDate (DateTime date)
+        {
+            SystemTime updatedTime = new SystemTime();
+            updatedTime.Year = (ushort)date.Year;
+            updatedTime.Month = (ushort)date.Month;
+            updatedTime.Day = (ushort)date.Day;
+            updatedTime.Hour = (ushort)0;
+            updatedTime.Minute = (ushort)0;
+            updatedTime.Second = (ushort)0;
+
+            Win32SetSystemTime(ref updatedTime);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //SetNewDate(new DateTime(2009,2,2));
+
+            Initialize();
+            StartMyTimer();
         }
     }
 }
